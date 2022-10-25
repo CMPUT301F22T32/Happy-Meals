9dd63441363ecf9829715c7a878815fb1810016c
Sophia Ruduke <sruduke@ualberta.ca>
2022-10-25 16:05:55 -0600

    entry pages

--- app/src/main/java/com/example/happymeals/LoginScreen.java
+++ app/src/main/java/com/example/happymeals/LoginScreen.java
@@ -3,6 +3,10 @@ package com.example.happymeals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        TextView forgetPassword = findViewById(R.id.forgot_password);
+        Button login = findViewById(R.id.login_button);
+
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open new activity or fragment to deal with password reset
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // first check that username and password have been filled
+                EditText username = findViewById(R.id.input_username);
+                EditText password = findViewById(R.id.input_password);
+
+                if (username.getText().length() <= 0 || password.getText().length() <= 0) {
+                    // fragment pop-up saying fields are not filled
+                } else {
+                    // then check entered account against database
+                }
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/happymeals/MainActivity.java
+++ app/src/main/java/com/example/happymeals/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.happymeals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new MainActivityListAdapter(this, 0);
+        ListView options = findViewById(R.id.option_list);
+        options.setAdapter(adapter);
+
     }
 }

83a9425d7a2e675e4fb84b939f63092ace6277f1
Josh Eastgaard <jeastgaa@ualberta.ca>
2022-10-27 18:58:51 -0600

    Reverted accidental commit of main avtivity.

--- app/src/main/java/com/example/happymeals/MainActivity.java
+++ app/src/main/java/com/example/happymeals/MainActivity.java
@@ -4,26 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import java.util.Date;
-
-public class MainActivity extends AppCompatActivity implements DatabaseListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FireStoreManager fm = new FireStoreManager();
-
-        testClass tc = new testClass("Apple", 23, new Date() );
-
-        fm.addData("Ingredients", tc.getName(), tc);
-        fm.getData( "Ingredients", tc.getName(), this, new testClass());
-        fm.getData( fm.getReferenceTo("Ingredients", tc.getName()), this, new testClass());
-    }
-
-    @Override
-    public void onDataFetchSuccess(DatabaseObject data) {
-        System.out.println( data.getClass() );
     }
 }

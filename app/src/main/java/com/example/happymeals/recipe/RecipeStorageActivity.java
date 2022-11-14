bd3a3a143bd2f970fd26c0a0e7a27a21bc46daf3
BAFiogbe <brandonfiogbe@gmail.com>
2022-11-13 17:59:21 -0700

    Implementation

--- app/src/main/java/com/example/happymeals/recipe/RecipeStorageActivity.java
+++ app/src/main/java/com/example/happymeals/recipe/RecipeStorageActivity.java
@@ -2,6 +2,7 @@ package com.example.happymeals.recipe;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -10,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.happymeals.DatasetWatcher;
 import com.example.happymeals.R;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.Comparator;
 
 /**
  * Class that holds the {@link AppCompatActivity} which will run the activity for the user.
@@ -32,10 +36,54 @@ public class RecipeStorageActivity extends AppCompatActivity implements DatasetW
         recipeStorage.setListeningActivity(this);
         adapter = new RecipeStorageAdapter(this, recipeStorage.getRecipes());
         recipeListView.setAdapter(adapter);
-        Spinner RecipeFilter = (Spinner) findViewById(R.id.recipe_filter);
+
+        Spinner RecipeFilter = findViewById(R.id.recipe_filter);
+        //String selectedSort = RecipeFilter.getSelectedItem().toString();
+        int selectCurrent = RecipeFilter.getSelectedItemPosition();
+
+
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(RecipeStorageActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.dropdown_options));
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         RecipeFilter.setAdapter(dataAdapter);
+        // TODO finish Recipe Sort
+        RecipeFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0:
+                        
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        adapter.sort(new Comparator<Recipe>(){
+
+                        @Override
+                        public int compare(Recipe recipe1, Recipe recipe2) {
+                            return recipe1.getPrepTime().compareTo(recipe2.getPrepTime());
+                        }
+                    });
+                        break;
+
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    })
+
+
     }
 
 

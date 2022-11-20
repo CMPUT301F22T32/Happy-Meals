0204e4fecac5140324574b4ee9884df06fe3a8f6
BAFiogbe <brandonfiogbe@gmail.com>
2022-11-19 21:07:05 -0700

    Update RegisterActivity.java

--- app/src/main/java/com/example/happymeals/userlogin/RegisterActivity.java
+++ app/src/main/java/com/example/happymeals/userlogin/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -64,7 +65,8 @@ public class RegisterActivity extends AppCompatActivity{
 
                     @Override
                     public void onFailure(Exception e) {
-                        return;}
+                        Log.d("RegisterActivity", "User could not be created." );
+                        }
                     });
 
                 }

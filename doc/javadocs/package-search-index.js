d0aede4e5a6846d90580da9affc9b738bb0f14ac
Brandon <68797818+BAFiogbe@users.noreply.github.com>
2022-11-21 14:07:41 -0700

    Revert "Merge branch 'master' of https://github.com/CMPUT301F22T32/Happy-Meals"
    This reverts commit 27d552f2e96dbf6c25c8cfadd4b1af3fb8ae6827.

--- doc/javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.happymeals"},{"l":"com.example.happymeals.database"},{"l":"com.example.happymeals.fragments"},{"l":"com.example.happymeals.ingredient"},{"l":"com.example.happymeals.mealplan"},{"l":"com.example.happymeals.recipe"}]

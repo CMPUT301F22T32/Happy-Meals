f8f98aa0a01ab5ec0ff0f147d9f26a8522085a2e
kstark1 <kstark@ualberta.ca>
2022-11-27 17:22:57 -0700

    Add ingredient intent test #97

---
+++ app/src/androidTest/java/com/example/happymeals/userlogin/AddIngredientIntentTesting.java
@@ -0,0 +1,344 @@
+package com.example.happymeals.userlogin;
+
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.happymeals.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AddIngredientIntentTesting {
+
+    @Rule
+    public ActivityScenarioRule<StartScreenActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(StartScreenActivity.class);
+
+    @Test
+    public void addIngredientIntentTesting() {
+         /*// Must Already be logged in!!
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.login_redirect), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.input_username),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("test@test.com"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.input_password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText2.perform(click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.input_password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("123456789"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.input_password), withText("123456789"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText4.perform(click());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.input_password), withText("123456789"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText5.perform(click());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.input_password), withText("123456789"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText6.perform(pressImeActionButton());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.login_button), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                7),
+                        isDisplayed()));
+        materialButton2.perform(click()); */
+
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.ingredient_menu),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                2),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.add_new_ingredient_button),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.ing_view_save_button), withText("Save"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        12),
+                                1)));
+        materialButton3.perform(scrollTo(), click());
+
+        // error fragment pops up
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        materialButton4.perform(scrollTo(), click());
+
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.ing_content_name_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.scrollView2),
+                                        0),
+                                1)));
+        appCompatEditText7.perform(scrollTo(), replaceText("Cookie"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.ing_content_desc_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.scrollView2),
+                                        0),
+                                3)));
+        appCompatEditText8.perform(scrollTo(), replaceText("Chocolate Chip Cookies"), closeSoftKeyboard());
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.ingredient_date_button), withText("DATE"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        5),
+                                1)));
+        materialButton5.perform(scrollTo(), click());
+
+        // open date picker
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withClassName(is("androidx.appcompat.widget.AppCompatImageButton")), withContentDescription("Next month"),
+                        childAtPosition(
+                                allOf(withClassName(is("android.widget.DayPickerView")),
+                                        childAtPosition(
+                                                withClassName(is("com.android.internal.widget.DialogViewAnimator")),
+                                                0)),
+                                2)));
+        appCompatImageButton.perform(scrollTo(), click());
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        materialButton6.perform(scrollTo(), click());
+
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.ing_content_unit_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        7),
+                                1)));
+        appCompatSpinner.perform(scrollTo(), click());
+
+        DataInteraction materialTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        materialTextView.perform(click());
+
+        ViewInteraction appCompatEditText10 = onView(
+                allOf(withId(R.id.ing_content_quantity_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        7),
+                                0)));
+        appCompatEditText10.perform(scrollTo(), click());
+
+        ViewInteraction appCompatEditText11 = onView(
+                allOf(withId(R.id.ing_content_quantity_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        7),
+                                0)));
+        appCompatEditText11.perform(scrollTo(), replaceText("250"), closeSoftKeyboard());
+
+        ViewInteraction appCompatSpinner2 = onView(
+                allOf(withId(R.id.ing_content_location_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.scrollView2),
+                                        0),
+                                9)));
+        appCompatSpinner2.perform(scrollTo(), click());
+
+        DataInteraction materialTextView2 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        materialTextView2.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(android.R.id.text1), withText("Fridge"),
+                        withParent(allOf(withId(R.id.ing_content_location_input),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Fridge")));
+
+        ViewInteraction appCompatSpinner3 = onView(
+                allOf(withId(R.id.ing_content_category_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.scrollView2),
+                                        0),
+                                11)));
+        appCompatSpinner3.perform(scrollTo(), click());
+
+        DataInteraction materialTextView3 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(4);
+        materialTextView3.perform(click());
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(android.R.id.text1), withText("Snack"),
+                        withParent(allOf(withId(R.id.ing_content_category_input),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        textView3.check(matches(withText("Snack")));
+
+        ViewInteraction materialButton7 = onView(
+                allOf(withId(R.id.ing_view_save_button), withText("Save"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        12),
+                                1)));
+        materialButton7.perform(scrollTo(), click());
+
+        ViewInteraction materialButton8 = onView(
+                allOf(withId(android.R.id.button1), withText("Confirm"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        materialButton8.perform(scrollTo(), click());
+
+        // new item is in list
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.ingredient_storage_list_name_field), withText("Cookie"),
+                        withParent(withParent(withId(R.id.storage_list))),
+                        isDisplayed()));
+        textView4.check(matches(withText("Cookie")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
